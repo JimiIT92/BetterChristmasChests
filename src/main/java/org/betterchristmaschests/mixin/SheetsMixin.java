87c5111635100e68bb0c1dfbd9213b1b75f292af
JimiIT92 <jimiit92@gmail.com>
2025-10-01 22:21:43 +0200

    Updated to 1.21.9 + added christmas copper chest textures

--- src/main/java/org/betterchristmaschests/mixin/SheetsMixin.java
+++ src/main/java/org/betterchristmaschests/mixin/SheetsMixin.java
@@ -1,12 +1,15 @@
 package org.betterchristmaschests.mixin;
 
 import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.renderer.blockentity.ChestRenderer;
+import net.minecraft.client.renderer.blockentity.state.ChestRenderState;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.EnderChestBlockEntity;
 import net.minecraft.world.level.block.state.properties.ChestType;
 import org.betterchristmaschests.BetterChristmasChests;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -20,18 +23,39 @@ public class SheetsMixin {
     /**
      * Render the Christmas Ender Chest Texture when it's Christmas
      *
-     * @param blockEntity The {@link BlockEntity Chest Block Entity}
-     * @param chestType The {@link ChestType Chest Type}
-     * @param holiday {@link Boolean Whether it's Christmas}
+     * @param variant The {@link ChestRenderState.ChestMaterialType Chest Variant}
+     * @param type The {@link ChestType Chest Type}
      * @param callbackInfoReturnable The {@link CallbackInfoReturnable<Material> Material Callback Info Returnable}
      */
-    @Inject(at = @At(value = "RETURN"), method = "chooseMaterial(Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/level/block/state/properties/ChestType;Z)Lnet/minecraft/client/resources/model/Material;", cancellable = true)
-    private static void chooseMaterial(final BlockEntity blockEntity, final ChestType chestType, final boolean holiday, final CallbackInfoReturnable<Material> callbackInfoReturnable) {
-        if(holiday) {
-            if (blockEntity instanceof EnderChestBlockEntity) {
-                callbackInfoReturnable.setReturnValue(new Material(Sheets.CHEST_SHEET, BetterChristmasChests.resourceLocation("entity/chest/ender")));
+    @Inject(at = @At(value = "RETURN"), method = "chooseMaterial(Lnet/minecraft/client/renderer/blockentity/state/ChestRenderState$ChestMaterialType;Lnet/minecraft/world/level/block/state/properties/ChestType;)Lnet/minecraft/client/resources/model/Material;", cancellable = true)
+    private static void chooseMaterial(final ChestRenderState.ChestMaterialType variant, final ChestType type, final CallbackInfoReturnable<Material> callbackInfoReturnable) {
+        if(ChestRenderer.xmasTextures()) {
+            switch (variant) {
+                case ENDER_CHEST -> callbackInfoReturnable.setReturnValue(getChestId("ender", type));
+                case COPPER_UNAFFECTED -> callbackInfoReturnable.setReturnValue(getChestId("copper", type));
+                case COPPER_WEATHERED -> callbackInfoReturnable.setReturnValue(getChestId("copper_weathered", type));
+                case COPPER_EXPOSED -> callbackInfoReturnable.setReturnValue(getChestId("copper_exposed", type));
+                case COPPER_OXIDIZED -> callbackInfoReturnable.setReturnValue(getChestId("copper_oxidized", type));
+                default -> callbackInfoReturnable.setReturnValue(callbackInfoReturnable.getReturnValue());
             }
         }
+    }
+
+    /**
+     * Get the {@link Material chest sprite identifier}
+     *
+     * @param name The {@link String base chest identifier name}
+     * @param type The {@link ChestType chest type}
+     * @return The {@link Material chest sprite identifier}
+     */
+    @Unique
+    private static Material getChestId(final String name, final ChestType type) {
+        final String suffix = switch (type) {
+            case SINGLE -> "";
+            case LEFT -> "_left";
+            case RIGHT -> "_right";
+        };
+        return new Material(Sheets.CHEST_SHEET, BetterChristmasChests.resourceLocation("entity/chest/" + name + suffix));
     }
 
 }
